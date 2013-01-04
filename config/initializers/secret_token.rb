d583404fadb293f2e38d95cb4a9afc4092b6c376
Roberto Bartolome <roberto.bartolome.medina@gmail.com>
2013-01-04 23:10:45 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lana2::Application.config.secret_token = 'f9cdf38f5e41d95fa9275697066755d0fb39cfe1a88ec74e96ef4d1940ace69c931c4ddeaf09398e8f436858271ed1d7584c1df209d3b3b88b7701093f92de71'
